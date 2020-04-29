9606deed1471bc03822b1e90299a7be0dcdf1e9a
Noopur <Noopur@DESKTOP-9EDLS42>
2020-04-29 13:28:12 +0530

    First Commit

---
+++ RestProvider/src/test/java/com/discover/RestProvider/RestProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discover.RestProvider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
