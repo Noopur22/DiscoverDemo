e615c135a513136196901917d0aed7a2929ccd5b
Noopur <Noopur@DESKTOP-9EDLS42>
2020-04-29 15:52:42 +0530

    Created Rest Consumer

---
+++ RestConsumer/src/main/java/com/discover/RestConsumer/RestConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.discover.RestConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestConsumerApplication.class, args);
+	}
+
+}
---
+++ RestConsumer/src/test/java/com/discover/RestConsumer/RestConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discover.RestConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
